95e3500240345f8b081ebae6c5a4fb04515bbce6
thatscoding <sachinshakya205001@gmail.com>
2025-04-02 08:12:10 +0530

    added : rombus

--- module-1/2-output-basic-data-types/assignments/print-rombus/Main.java
+++ module-1/2-output-basic-data-types/assignments/print-rombus/Main.java
@@ -1,10 +1,27 @@
-public class Main{
+
+public class Main {
+
     public static void main(String[] args) {
         int n = 3;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == 3) {
+                    if (j == 3) {
+                        break;
+                    } else if (j == 2) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                } else {
+                    if ((i + j) % 2 != 0) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
             }
+            System.out.println();
         }
     }
 }
